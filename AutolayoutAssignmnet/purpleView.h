401f909e2c9a20bc68cafa13e2ae514d73ebf009
Nicholas Fung <nbfung@sfu.ca>
2017-10-10 15:45:58 -0700

    Fixed Autolayout Constraints

---
+++ AutolayoutAssignmnet/purpleView.h
@@ -0,0 +1,13 @@
+//
+//  purpleView.h
+//  AutolayoutAssignmnet
+//
+//  Created by Nicholas Fung on 2017-10-10.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface purpleView : UIViewController
+
+@end
---
+++ AutolayoutAssignmnet/purpleView.m
@@ -0,0 +1,32 @@
+//
+//  purpleView.m
+//  AutolayoutAssignmnet
+//
+//  Created by Nicholas Fung on 2017-10-10.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import "purpleView.h"
+
+@implementation purpleView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
